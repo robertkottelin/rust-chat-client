66e4954e545eecc6c94db378581dc7de369f74ed
robertkottelin <robert.kottelin@gmail.com>
2023-03-31 16:38:29 +0200

    not working

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,19 @@
-use tokio::{
-    io::{AsyncBufReadExt, AsyncWriteExt, BufReader},
-    net::TcpStream,
-};
+use std::io::*;
+use std::net::TcpStream;
 
-#[tokio::main]
-async fn main() {
-    let server_addr = "SERVER_IP:8080"; // Replace SERVER_IP with your server's IP address
-    let mut stream = TcpStream::connect(server_addr).await.unwrap();
+fn main() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
 
-    let (mut reader, mut writer) = stream.split();
-    let mut reader = BufReader::new(reader);
+    let mut serverinput = stream.read(&mut [0; 128])?;
 
-    // Spawn a task to read incoming messages from the server
-    tokio::spawn(async move {
-        let mut buffer = String::new();
-        loop {
-            match reader.read_line(&mut buffer).await {
-                Ok(bytes) if bytes > 0 => {
-                    println!("{}", buffer.trim());
-                    buffer.clear();
-                }
-                Ok(_) => break, // EOF or the server closed the connection
-                Err(e) => {
-                    eprintln!("Error reading from server: {}", e);
-                    break;
-                }
-            }
-        }
-    });
+    println!("{:?}", serverinput);
 
-    // Read user input and send it to the server
-    let mut input = String::new();
-    while let Ok(_) = std::io::stdin().read_line(&mut input) {
-        if let Err(e) = writer.write_all(input.as_bytes()).await {
-            eprintln!("Error sending message: {}", e);
-            break;
-        }
-        input.clear();
-    }
+    let stdin = stdin();
+
+    let mut userinput = String::new();
+    stdin.lock().read_line(&mut userinput)?;
+
+    stream.write(userinput.as_bytes())?;
+    
+    Ok(())
 }
