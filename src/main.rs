ea2aa9ec3d412cdfb524fee55ae766e50f898695
robertkottelin <robert.kottelin@gmail.com>
2023-04-01 11:49:05 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::io::*;
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::net::TcpStream;
+use std::io::stdin;
 
 fn main() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:8080")?;
+    let stream = TcpStream::connect("127.0.0.1:8080")?;
 
-    let mut serverinput = stream.read(&mut [0; 128])?;
+    // Create a buffered reader and writer for the stream
+    let mut reader = BufReader::new(&stream);
+    let mut writer = BufWriter::new(&stream);
 
-    println!("{:?}", serverinput);
+    loop {
+        // Read incoming data from the server
+        let mut server_data = String::new();
+        reader.read_line(&mut server_data)?;
+        println!("Server: {}", server_data);
 
-    let stdin = stdin();
+        // Read user input from the console
+        let mut user_input = String::new();
+        stdin().read_line(&mut user_input)?;
 
-    let mut userinput = String::new();
-    stdin.lock().read_line(&mut userinput)?;
+        // If the user types "exit", close the connection and exit the program
+        if user_input.trim().eq_ignore_ascii_case("exit") {
+            break;
+        }
 
-    stream.write(userinput.as_bytes())?;
-    
+        // Send user input back to the server
+        writer.write_all(user_input.as_bytes())?;
+        writer.flush()?;
+    }
+
     Ok(())
 }
