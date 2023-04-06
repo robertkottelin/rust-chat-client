b3b761501de15e969d3083155dcc001ce9018dee
robertkottelin <robert.kottelin@gmail.com>
2023-04-06 16:04:40 +0200

    push for merge

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::io;
+use anyhow::{Result};
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpStream;
 use tokio::task;
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
+async fn main() -> Result<()> {
     let stream = TcpStream::connect("localhost:8080").await?;
 
     // Create a buffered reader and writer for the stream
@@ -18,7 +19,10 @@ async fn main() -> io::Result<()> {
             match reader.read_line(&mut server_data).await {
                 Ok(n) if n == 0 => continue, // empty line, continue loop
                 Ok(_) => println!("{}", server_data), // line read successfully
-                Err(e) => panic!("Error reading from server: {}", e), // error reading from server
+                Err(e) => {
+                    eprintln!("Error reading from server: {}", e);
+                    break;
+                }
             }
         }
     });
@@ -27,26 +31,29 @@ async fn main() -> io::Result<()> {
         // Write to server
         loop {
             let mut user_input = String::new();
-            io::stdin().read_line(&mut user_input).unwrap();
-            //let msg = format!();
-    
+            if io::stdin().read_line(&mut user_input).is_err() {
+                eprintln!("Error reading user input");
+                break;
+            }
+            // let msg = format!("Sent: {:?}", user_input);
+
             match writer.write_all(user_input.as_bytes()).await {
                 Ok(_) => {
                     if let Err(e) = writer.flush().await {
-                        println!("Error flushing writer: {}", e);
+                        eprintln!("Error flushing writer: {}", e);
                         break; // exit loop if there is an error flushing the writer
                     }
                 },
                 Err(e) => {
-                    println!("Error writing to server: {}", e);
+                    eprintln!("Error writing to server: {}", e);
                     break; // exit loop if there is an error writing to the server
                 }
             }
         }
     });
-    
+
     // Wait for all tasks to complete
-    let _ = tokio::try_join!(read, write);
+    let _ = tokio::try_join!(read, write)?;
 
     Ok(())
 }
